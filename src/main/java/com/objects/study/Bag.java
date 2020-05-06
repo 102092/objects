8a467202d798ac997492c5757e5888bc76a2d720
Han <quaint3@gmail.com>
2020-05-06 23:14:46 +0900

    feature : Audience 의 Bag 클래스를 자율적인 존재로 변경

--- src/main/java/com/objects/study/Bag.java
+++ src/main/java/com/objects/study/Bag.java
@@ -6,32 +6,27 @@ public class Bag {
   private Invitation invitation;
   private Ticket ticket;
 
-  public Bag(long amount) {
-    this(null, amount);
+  public Long hold(Ticket ticket) {
+    if(hasInvitation()) {
+      setTicket(ticket);
+      return 0L;
+    } else {
+      setTicket(ticket);
+      minusAmount(ticket.getFee());
+      return ticket.getFee();
+    }
   }
 
-  public Bag(Invitation invitation, long amount) {
-    this.amount = amount;
-    this.invitation = invitation;
-  }
-
-  public boolean hasInvitation() {
+  private boolean hasInvitation() {
     return invitation != null;
   }
 
-  public boolean hasTicket() {
-    return ticket != null;
-  }
 
-  public void minusAmount(Long amount) {
+  private void minusAmount(Long amount) {
     this.amount -= amount;
   }
 
-  public void plusAmount(Long amount) {
-    this.amount += amount;
-  }
-
-  public void setTicket(Ticket ticket) {
+  private void setTicket(Ticket ticket) {
     this.ticket = ticket;
   }
 }
